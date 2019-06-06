d98268f4aff9bad31f301b52eca4726447698efa
PavelTishenko <32521236+PavelTishenko@users.noreply.github.com>
2019-06-06 12:12:39 +0300

    add template literals and deal with some mess in components like checkbox and input

--- src/app/components/checkbox.component.ts
+++ src/app/components/checkbox.component.ts
@@ -2,25 +2,19 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
-  template: '<input type="checkbox" (change)="checkedOrNo()" />' +
-            '<span *ngIf="valOfCheck">DONE in this date: {{day}}.{{month}}.{{year}}</span>'
+  template: `<input type="checkbox" (change)="checkedOrNo()" />
+             <span *ngIf="valOfCheck">DONE in this date: {{day}}.{{month}}.{{year}}</span>`
 })
 
 export class CheckboxComponent implements OnInit {
-
-
   valOfCheck = false;
   date = new Date();
   day: number = this.date.getDay();
   month: number = this.date.getMonth();
   year: number = this.date.getFullYear();
   constructor() {}
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
   checkedOrNo() {
-    this.valOfCheck = !this.valOfCheck;
-  }
+    this.valOfCheck = !this.valOfCheck; }
 }
 
--- src/app/components/input.component.ts
+++ src/app/components/input.component.ts
@@ -4,34 +4,20 @@ import {CheckboxComponent} from './checkbox.component';
 
 @Component({
   selector: 'app-input',
-  template: '' +
-    '<form>' +
-    '<input type="text" (change)="valOfInput($event)"/> ' +
-    '<input type="submit" (click)="addToList($event)"/> ' +
-    '</form> ' +
-    '<ul style="list-style: none">' +
-    '<li *ngFor="let el of listVal">{{ el }}<app-checkbox></app-checkbox></li>' +
-    '</ul>'
-
+  template: `<form><input type="text" (change)="valOfInput($event)"/><input type="submit" (click)="addToList($event)"/></form>
+    <ul style="list-style: none">
+    <li *ngFor="let el of listVal">{{ el }}<app-checkbox></app-checkbox></li>
+  </ul>`
 })
-
 export class InputComponent implements  OnInit {
  constructor() {}
-
-  listVal: string[] = [];
-  valInput: string;
-
-
-
+ listVal: string[] = [];
+ valInput: string;
  ngOnInit(): void {
  }
-
-  valOfInput(event) {
-    this.valInput = event.target.value;
-  }
-
-  addToList(event) {
-     event.preventDefault();
-     this.listVal.push(this.valInput);
- }
+ valOfInput(event) {
+   this.valInput = event.target.value; }
+ addToList(event) {
+   event.preventDefault();
+   this.listVal.push(this.valInput); }
 }
